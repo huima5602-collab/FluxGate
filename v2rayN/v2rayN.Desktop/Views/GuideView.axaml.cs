9882d3594d61f1b3bb0b34837791282251285929
huima5602-collab <huima5602@gmail.com>
2026-06-20 00:57:11 +0800

    sync v2rayN/v2rayN.Desktop/Views/GuideView.axaml.cs for v0.2.0-ui-preview.3

---
+++ v2rayN/v2rayN.Desktop/Views/GuideView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace v2rayN.Desktop.Views
+{
+    public partial class GuideView : UserControl
+    {
+        public GuideView()
+        {
+            InitializeComponent();
+        }
+    }
+}
