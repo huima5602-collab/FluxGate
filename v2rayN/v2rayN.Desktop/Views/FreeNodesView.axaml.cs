a90109ad627ce46184ae1148018167bb1dd2bccc
huima5602-collab <huima5602@gmail.com>
2026-06-20 14:12:17 +0800

    sync v2rayN/v2rayN.Desktop/Views/FreeNodesView.axaml.cs for v0.2.0-ui-preview.4

--- v2rayN/v2rayN.Desktop/Views/FreeNodesView.axaml.cs
+++ v2rayN/v2rayN.Desktop/Views/FreeNodesView.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using v2rayN.Desktop.Common;
 using ServiceLib.Common;
 
 namespace v2rayN.Desktop.Views
@@ -12,11 +13,23 @@ namespace v2rayN.Desktop.Views
         {
             InitializeComponent();
             btnOpenFreeNodes.Click += OpenFreeNodes_Click;
+            btnReloadFreeNodes.Click += ReloadFreeNodes_Click;
+            btnCopyFreeNodesUrl.Click += CopyFreeNodesUrl_Click;
         }
 
         private void OpenFreeNodes_Click(object? sender, RoutedEventArgs e)
         {
             ProcUtils.ProcessStart(FreeNodesUrl);
+        }
+
+        private void ReloadFreeNodes_Click(object? sender, RoutedEventArgs e)
+        {
+            webFreeNodes.Source = new Uri(FreeNodesUrl);
+        }
+
+        private async void CopyFreeNodesUrl_Click(object? sender, RoutedEventArgs e)
+        {
+            await AvaUtils.SetClipboardData(this, FreeNodesUrl);
         }
     }
 }
